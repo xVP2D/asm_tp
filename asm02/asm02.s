b06ff89b29032e63ec450ddc62f3fd95d54b673b
granola9025 <chochochouquette@gmail.com>
2026-09-04 18:03:43 +0200

    test

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,31 +1,35 @@
 section .data
-    msg db "1337", 0ah
-    buf db "  "
+ msg db "1337", 0ah
+ buf db "  "
 
 section .text
-  global _start
+ global _start
 
 _start:
- mov rax, 0    
- mov rdi, 0      
- mov rsi, buf 
- mov rdx, 2              
+ mov rax, 0
+ mov rdi, 0
+ mov rsi, buf
+ mov rdx, 2
  syscall
 
 
- cmp ax, bx
- je     Equal
+ cmp byte [buf], '4'
+ jne fail
+ cmp byte [buf+1], '2'
+ jne fail
+
+
  mov rax, 1
  mov rdi, 1
  mov rsi, msg
  mov rdx, 5
  syscall
+
  mov rax, 60
  mov rdi, 0
- jl     Less
-
-
- mov rax, 60
-
  syscall
-  
+
+fail:
+ mov rax, 60
+ mov rdi, 1
+ syscall
