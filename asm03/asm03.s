35bdd8ed2cc79df4dfff33660bea19dd683de179
granola9025 <chochochouquette@gmail.com>
2026-09-04 19:25:44 +0200

    lets goooooooooooooo

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -7,23 +7,22 @@ section .text
 _start:
  mov rax, [rsp]
  cmp rax, 2
- jl fail
+ jne fail
 
  mov rsi, [rsp + 16] 
 
- cmp byte rsi, '42'
- jl fail
+ cmp word  [rsi], '42'
+ jne fail
 
  mov rax, 1
  mov rdi, 1
  mov rsi, msg
- mov rdx, 5
+ mov rdx, 10
  syscall
 
  mov rax, 60
  mov rdi, 0
  syscall
- jne fail
 
 fail:
  mov rax, 60
