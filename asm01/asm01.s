f52d4b1a5d0c8b9c31a0cacd1f62cc0badb47a1e
granola9025 <chochochouquette@gmail.com>
2026-09-04 15:49:13 +0200

    gitignore exo2

---
+++ asm01/asm01.s
@@ -0,0 +1,18 @@
+section .data
+    msg db "1337", 0ah
+
+section .text
+  global _start
+
+_start:
+ mov rax, 1
+ mov rdi, 1
+ mov rsi, msg
+ mov rdx, 5
+ syscall
+
+ mov rax, 60
+ mov rdi, 0
+
+ syscall
+  
